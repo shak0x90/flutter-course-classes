67637e404346f60e53025330517737691d57e6e8
shakil <hridoyhussainshakil@gmail.com>
2021-09-21 11:49:33 +0600

    day9

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:testapp/homePage.dart';
+//import 'package:testapp/homePage.dart';
+import 'package:testapp/profileScreen.dart';
 
 void main() {
   runApp(
     MaterialApp(
-        title: "Demo App",
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(fontFamily: "Lobster"),
-        home: HomePage()),
+      title: "Demo App",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: "Lobster"),
+      // home: HomePage()),
+      home: ProfileScreen(),
+    ),
   );
 }
